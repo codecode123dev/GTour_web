3842dcb37c70de349d792db5960aff9d0f9c1537
minh <minhnvgch200213@fpt.edu.vn>
2023-09-29 16:10:35 +0700

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# GTour_web
--- gtour_web/src/App.js
+++ gtour_web/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer_Tour from './Components/Footer_Tour';
+import Header_Tour from './Components/Header_Tour';
+import Home from './Views/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header_Tour/>
+        <Home/>
+      <Footer_Tour/>
+    </>
   );
 }
 
