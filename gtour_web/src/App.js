b12938e54cbe5c5418a1c6d7f3a7da589a33b2fb
minh <minhnvgch200213@fpt.edu.vn>
2023-10-06 09:57:17 +0700

    update

--- gtour_web/src/App.js
+++ gtour_web/src/App.js
@@ -1,11 +1,15 @@
 import Footer_Tour from './Components/Footer_Tour';
 import Header_Tour from './Components/Header_Tour';
+import Container1 from './Views/Container1';
+import Container2 from './Views/Container2';
 import Home from './Views/Home';
 
 function App() {
   return (
     <>
-      <Header_Tour/>
+      {/* <Header_Tour/> */}
+      <Container1/>
+      <Container2/>
         <Home/>
       <Footer_Tour/>
     </>
